2a1f3a0ded92ed911866c5a2526211a66b9fd960
Eric Snyder <guideon72@hotmail.com>
2022-11-09 15:23:53 -0800

    Adding updated todo to include working match case block

--- todo.py
+++ todo.py
@@ -13,19 +13,40 @@ taskList = []
 #         # return taskList
 #     else:
 #         return str("Sorry, I don't recognize that task. Please try again")
+#
+# def getTask(task):
+#    if re.match(pattern, task):
+#         return task
+#     return ("I'm sorry, that is not a valid task")
 
 
 def main():
     repeat = True
     while repeat:
-        nTask = input(
-            'Please enter your new task here: (Type "quit" to exit) ')
-        if nTask == "quit":
+        uAction = input(
+            '"Add" a task, "See" your todo list or Edit a task? (Type "exit" to quit) ').strip().lower()
+        # Checking exit here instead of in match case to provide an earlier 'out' TODO: remove this and reorder case block w/ exit at top
+        if uAction == "exit":
             repeat = False
             break
         else:
-            taskList.append(nTask)
-            print(taskList)
+            match uAction:
+                case "add":
+                    nTask = input("Please enter your new task now: ")
+                    # refactor validation to something less obnoxious
+                    taskList.append(nTask.title())
+                case "see":
+                    for t in taskList:
+                        print(t)
+                case "edit":
+                    option = input("Enter the task you want to edit: ")
+                    for i in range(len(taskList)):
+                        if taskList[i].lower() == option.lower():
+                            nt = input(
+                                F"What would you like to replace {taskList[i]} with? ")
+                            taskList[i] = nt.title()
+                case _:
+                    print("Please type, either, 'Add' or 'See'")
 
 
 
