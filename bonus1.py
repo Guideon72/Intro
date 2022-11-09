cb7466b819475a726cb894427b73a6a0e18d3e71
Eric Snyder <guideon72@hotmail.com>
2022-11-08 16:58:05 -0800

    Adding bonus1.py to repo

---
+++ bonus1.py
@@ -0,0 +1,5 @@
+"""Bonus exercise to input a string, count the number of characters in the string and print that value out to the command line"""
+
+title = input("Please input the title of your book: ")
+
+print(f"Your book's title is {len(title)} characters in length.")
