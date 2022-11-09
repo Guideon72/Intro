ccac75ea9d0be8c5561dc3b87a03218ce46b1289
Eric Snyder <guideon72@hotmail.com>
2022-11-08 18:04:19 -0800

    Adding latest version of todo.py running in a while loop

--- todo.py
+++ todo.py
@@ -1,24 +1,32 @@
 """A basic ToDo list application written in Python 3.10"""
 import re
 
-task = input('Please enter your new task here: ')
-pattern = "^[A-Za-z0-9', ]*$"
+# task = input('Please enter your new task here: ')
+# pattern = "^[A-Za-z0-9', ]*$"
 taskList = []
 
 
-def getTask(task):
-    if re.match(pattern, task):
-        global taskList
-        taskList.append(task)
-        # return taskList
-    else:
-        return str("Sorry, I don't recognize that task. Please try again")
+# def getTask(task):
+#     if re.match(pattern, task):
+#         global taskList
+#         taskList.append(task)
+#         # return taskList
+#     else:
+#         return str("Sorry, I don't recognize that task. Please try again")
 
 
 def main():
-    nt = getTask(task)
-    print(nt)
-    print(taskList)
+    repeat = True
+    while repeat:
+        nTask = input('Please enter your new task here: ')
+        if nTask == "quit":
+            repeat = False
+            break
+        else:
+            taskList.append(nTask)
+            print(taskList)
+
+
 
 
 if __name__ == '__main__':
