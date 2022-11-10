ea539590e48a25d3f3bd1bc8c55caded5a11b7e6
Eric Snyder <guideon72@hotmail.com>
2022-11-09 17:10:56 -0800

    Updated currencyconvert.py to get symbol and currency name

--- currencyconvert.py
+++ currencyconvert.py
@@ -1,23 +1,22 @@
-"""Bonus exercise to input a string, count the number of characters in the string and print that value out to the command line"""
-
-# title = input("Please input the title of your book: ")
-
-# print(f"Your book's title is {len(title)} characters in length.")
-# """___________________________________________________________________________________"""
 from currencies import Currency
 from currency_converter import CurrencyConverter
 
 
 def getCurrency(curr, amt):
     nCurr = Currency(curr)
-    cash = nCurr.get_money_format(amt)
+    cash = nCurr.get_money_with_currency_format(amt)
     return cash
 
 
-conv = CurrencyConverter()
-money = input("How many dollars in USD do you have? ")
-changed = conv.convert(money, 'USD', "EUR")
+def main():
+    conv = CurrencyConverter()
+    money = input("How many dollars in USD do you have? ")
+    changed = conv.convert(money, 'USD', "EUR")
 
-owned = getCurrency('USD', money)
-result = getCurrency('EUR', changed)
-print(f'If you have {owned} that is equivalent to {result}')
+    owned = getCurrency('USD', money)
+    result = getCurrency('EUR', changed)
+    print(f'If you have {owned} that is equivalent to {result}')
+
+
+if __name__ == '__main__':
+    main()
