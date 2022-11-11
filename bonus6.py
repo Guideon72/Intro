76e296f3ba1fee58ef06d5145ed295406535964e
Eric Snyder <guideon72@hotmail.com>
2022-11-10 20:18:11 -0800

    Updated bonus6.py to use PATH

--- bonus6.py
+++ bonus6.py
@@ -1,10 +1,19 @@
 """TODO: Make this work with pathlib"""
+from pathlib import Path
+
+filepath = Path(Path.cwd() / "data" / "b6")
+filepath.mkdir(parents=True, exist_ok=True)
+filenames = [Path("file1.txt"),
+             Path("file2.txt"), Path("file3.txt")]
+files = []
+for filename in filenames:
+    file = filepath/filename
+    file.touch(exist_ok=True)
+    files.append(file)
+# print(files)
 
 contents = ["This is the content for file1.txt",
             "This is the content for file2.txt", "This is the content for file3.txt"]
-filenames = ["./data/b6/file1.txt",
-             "./data/b6/file2.txt", "./data/b6/file3.txt"]
 
-for content, filename in zip(contents, filenames):
-    with open(filename, 'w+')as file:
-        file.write(content)
+for content, file in zip(contents, files):
+    file.write_text(content)
