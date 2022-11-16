61bd5eedb0f4f2dfb7e361a3bbda977ae10d896f
Eric Snyder <guideon72@hotmail.com>
2022-11-16 12:14:24 -0800

    Updating latest to include using external file for Add, Show and Edit

--- todo.py
+++ todo.py
@@ -1,42 +1,59 @@
 """A basic ToDo list application written in Python 3.10"""
 
+
 def main():
     repeat = True
     while repeat:
         uAction = input(
             '"Add", "See", "Edit", "Complete"? (Type "exit" to quit) ').strip().lower()
-        # Checking exit here instead of in match case to provide an earlier 'out' TODO: remove this and reorder case block w/ exit at top
+# NOTE: Checking exit here instead of in match case to provide an earlier 'out'
+# TODO: Remove this and reorder case block w/ exit at top
         if uAction.lower() == "exit" or uAction.lower() == "quit":
             repeat = False
             break
         else:
             match uAction:
+                # TODO: Break cases out into discrete functions
+                # TODO: Add Enchant spell-checker functionality
                 case "add":
                     nTask = input("Please enter your new task now: ")
-                    with open("./data/tl.txt", "r") as tl:  # TODO: Make this work with pathlib
+# TODO: Make this work with pathlib
+                    with open("./data/tl.txt", "r") as tl:
                         tasks = tl.readlines()
+# BUG: Does not allow proper names
                     tasks.append(nTask.capitalize() + "\n")
                     with open("./data/tl.txt", "w") as tl:
                         tl.writelines(tasks)
+
                 case "see" | "show":
                     with open("./data/tl.txt", "r") as tl:
                         for i, t in enumerate(tl, 1):
+                            t = t.strip('\n')
                             print(f"{i}: {t}")
-                case "edit":  # Using first input to find element, second input to update list
+# NOTE: Using first input to find element, second input to update list
+                case "edit":
+                    with open("./data/tl.txt", "r") as tl:
+                        tasks = tl.readlines()
+                    for i, t in enumerate(tasks, 1):
+                        t = t.strip('\n')
+                        print(f"{i}: {t}")
                     eTask = input(
                         "Enter number of the task you want to edit: ")
-                    for item in enumerate(taskList, 1):
+                    for item in enumerate(tasks, 1):
                         if item[0] == int(eTask):
+                            # BUG: Does not allow proper names
                             ntask = input(
-                                F"What would you like to change {item[1]} to? ").title()
-                            taskList[item[0]-1] = ntask
-                        else:
-                            pass  # Todo: Don't do this for production code
+                                F"What would you like to change {item[1]} to? ").capitalize()
+                            tasks[item[0]-1] = ntask + "\n"
+                    with open("./data/tl.txt", "w") as tl:
+                        tl.writelines(tasks)
+
                 case "complete":
                     cTask = int(input(
                         "Enter the number of the task you want to complete: "))
                     completed = taskList.pop(cTask-1)
                     print(F"{completed} is done.")
+
                 case _:
                     print("Please type, either, 'Add', 'See' or 'Exit")
 
